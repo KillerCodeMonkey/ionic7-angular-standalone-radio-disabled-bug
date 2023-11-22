fe9d6d055bc104ffdc20adc8a3441098d6ec5f3e
Bengt Weiße <bengtler@gmail.com>
2023-11-22 15:27:17 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonApp, IonRadio, IonRadioGroup } from '@ionic/angular/standalone';
 
 @Component({
+  imports: [IonApp, IonRadioGroup, IonRadio, FormsModule],
   selector: 'app-root',
+  standalone: true,
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  value = "1"
+
   constructor() {}
 }
